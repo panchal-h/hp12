d05ca480101fa46abb5e6b5bd05ff85bb04b9e82
Hardik Panchal <hardik.panchal@tatvasoft.com>
2019-02-28 12:48:48 +0530

    Change this constant to a 'static' read-only property.

--- SmartLibrary.Infrastructure/Constants.cs
+++ SmartLibrary.Infrastructure/Constants.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="Constants.cs" company="Caspian Pacific Tech">
+// <copyright file="staticants.cs" company="Caspian Pacific Tech">
 // Copyright (c) Caspian Pacific Tech. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
@@ -7,7 +7,7 @@
 namespace SmartLibrary.Infrastructure
 {
     /// <summary>
-    ///  This class is for Constants.
+    ///  This class is for staticants.
     /// </summary>
     /// <CreatedBy>Dhruvi Shah.</CreatedBy>
     /// <CreatedDate>5-Sept-2018.</CreatedDate>
@@ -16,136 +16,172 @@ namespace SmartLibrary.Infrastructure
         /// <summary>
         /// Maximum file size in bytes.
         /// </summary>
-        public const int MAXIMUM_FILE_UPLOAD_SIZE_BYTES = 5242880;
+        public static int MAXIMUM_FILE_UPLOAD_SIZE_BYTES
+        {
+            get
+            {
+                return 5242880;
+            }
+        }
 
         /// <summary>
         /// Maximum file size in kilo bytes.
         /// </summary>
-        public const int MAXIMUM_FILE_UPLOAD_SIZE_KILOBYTES = 5120;
+        public static int MAXIMUM_FILE_UPLOAD_SIZE_KILOBYTES
+        {
+            get
+            {
+                return 5120;
+            }
+        }
 
         /// <summary>
         /// Maximum file size in mb.
         /// </summary>
-        public const int MAXIMUM_FILE_UPLOAD_SIZE_MEGABYTES = 5;
+        public static int MAXIMUM_FILE_UPLOAD_SIZE_MEGABYTES
+        {
+            get
+            {
+                return 5;
+            }
+        }
 
         /// <summary>
         /// datatables GridPageSize.
         /// </summary>
-        public const int GridPageSize = 20;
+        public static int GridPageSize
+        {
+            get
+            {
+                return 20;
+            }
+        }
 
         /// <summary>
         /// ACTION_View.
         /// </summary>
-        public const string ACTION_VIEW = "View";
+        public static string ACTION_VIEW
+        {
+            get
+            {
+                return "View";
+            }
+        }
 
         /// <summary>
         /// ACTION_ADDUpdate.
         /// </summary>
-        public const string ACTION_ADDUPDATE = "Add_Update";
+        public static string ACTION_ADDUPDATE
+        {
+            get
+            {
+                return "Add_Update";
+            }
+        }
 
         /// <summary>
         /// ACTION_DELETE.
         /// </summary>
-        public const string ACTION_DELETE = "Delete";
+        public static string ACTION_DELETE { get { return "Delete"; } }
 
         /// <summary>
         /// Company Name
         /// </summary>
-        public const string COMPANY_NAME = "Dubai Economy Smart Library";
+        public static string COMPANY_NAME { get { return "Dubai Economy Smart Library"; } }
 
         /// <summary>
         /// Forgot Password
         /// </summary>
-        public const string FORGOT_PASSWORD = "Forgot Password";
+        public static string FORGOT_PASSWORD { get { return "Forgot Password"; } }
 
         /// <summary>
         /// Invite Customer
         /// </summary>
-        public const string INVITE_CUSTOMER = "Invite Customer";
+        public static string INVITE_CUSTOMER { get { return "Invite Customer"; } }
 
         /// <summary>
         /// Invite Customer
         /// </summary>
-        public const string INVITE_CUSTOMER_HEADER = "Welcome To The Smart Library";
+        public static string INVITE_CUSTOMER_HEADER { get { return "Welcome To The Smart Library"; } }
 
         /// <summary>
         /// Confirm Book Borrow
         /// </summary>
-        public const string CONFIRM_BOOK_BORROW = "Confirm Book Borrow";
+        public static string CONFIRM_BOOK_BORROW { get { return "Confirm Book Borrow"; } }
 
         /// <summary>
         /// Confirm Book Borrow
         /// </summary>
-        public const string CONFIRM_BOOK_BORROW_HEADER = "Your Book Is Confirmed";
+        public static string CONFIRM_BOOK_BORROW_HEADER { get { return "Your Book Is Confirmed"; } }
 
         /// <summary>
         /// Cancel Book Borrow
         /// </summary>
-        public const string CANCEL_BOOK_BORROW_HEADER = "Your Request Is Cancelled";
+        public static string CANCEL_BOOK_BORROW_HEADER { get { return "Your Request Is Cancelled"; } }
 
         /// <summary>
         /// Request of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_REQUEST = "Room Booking Request";
+        public static string ROOM_BOOKING_REQUEST { get { return "Room Booking Request"; } }
 
         /// <summary>
         /// Request of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_REQUEST_FOR_ADMIN = "Request of Room Booking ";
+        public static string ROOM_BOOKING_REQUEST_FOR_ADMIN { get { return "Request of Room Booking "; } }
 
         /// <summary>
         /// Approval of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_APPROVE = "Approval of Room Booking";
+        public static string ROOM_BOOKING_APPROVE { get { return "Approval of Room Booking"; } }
 
         /// <summary>
         /// Approval of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_APPROVE_HEADER = "Your Booking Is Confirmed";
+        public static string ROOM_BOOKING_APPROVE_HEADER { get { return "Your Booking Is Confirmed"; } }
 
         /// <summary>
         /// Cancellation of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_CANCEL = "Cancellation of Room Booking";
+        public static string ROOM_BOOKING_CANCEL { get { return "Cancellation of Room Booking"; } }
 
         /// <summary>
         /// Cancellation of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_CANCEL_HEADER = "Your Booking Is Cancelled";
+        public static string ROOM_BOOKING_CANCEL_HEADER { get { return "Your Booking Is Cancelled"; } }
 
         /// <summary>
         /// Reschedule of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_RESCHEDULE = "Reschedule of Room Booking";
+        public static string ROOM_BOOKING_RESCHEDULE { get { return "Reschedule of Room Booking"; } }
 
         /// <summary>
         /// Reschedule of Room Booking
         /// </summary>
-        public const string ROOM_BOOKING_RESCHEDULE_HEADER = "Room Booking Reschedule";
+        public static string ROOM_BOOKING_RESCHEDULE_HEADER { get { return "Room Booking Reschedule"; } }
 
         /// <summary>
         /// Book Overdue
         /// </summary>
-        public const string BOOK_OVERDUE = "Your Book is Overdue!";
+        public static string BOOK_OVERDUE { get { return "Your Book is Overdue!"; } }
 
         /// <summary>
         /// Borrow Book Request
         /// </summary>
-        public const string BORROWBOOK_REQUEST_FOR_ADMIN = "Borrow Book Request";
+        public static string BORROWBOOK_REQUEST_FOR_ADMIN { get { return "Borrow Book Request"; } }
 
         /// <summary>
         /// Borrow Book Request
         /// </summary>
-        public const string MESSAGE_NOTIFICATION = "Message From Admin";
+        public static string MESSAGE_NOTIFICATION { get { return "Message From Admin"; } }
 
         /// <summary>
         /// Borrow Book Request
         /// </summary>
-        public const string MESSAGE_NOTIFICATION_HEADER = "You Have A Message!";
+        public static string MESSAGE_NOTIFICATION_HEADER { get { return "You Have A Message!"; } }
 
         /// <summary>
         /// Book Available
         /// </summary>
-        public const string BOOK_AVAILABLE = "Book Available";
+        public static string BOOK_AVAILABLE { get { return "Book Available"; } }
     }
 }
